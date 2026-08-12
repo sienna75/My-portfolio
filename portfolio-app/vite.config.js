203ec216c0d56cf6c8f4b9d29851b2816d815b8f
sienna75 <sienna.ngangastudent.moringaschool.com>
2026-08-12 02:58:13 -0700

    fix: remove duplicate import statements in vite.config.js

--- portfolio-app/vite.config.js
+++ portfolio-app/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/My-portfolio/',
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/My-portfolio/',
 })
